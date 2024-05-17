9cf67d89ecb54d3f20bb4cc4e25f4cbe7cb0606a
Fabri Cost <you@example.com>
2024-05-17 09:19:40 -0300

    Trabajo terminado

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 url = 'https://pypi.org/'
 
@@ -12,5 +13,19 @@ results = []
 
 for a in etiqueta_a:
     results.append(a['href'])
-print(results)
 
+data = {}
+
+for enlace in results:
+    try:
+        if enlace.startswith('http'):
+            respuesta = requests.get(enlace)
+            sopita = BeautifulSoup(respuesta.text, 'html.parser')
+            h1_etiqueta = [str(h1) for h1 in sopita.find_all('h1')]
+            p_etiqueta = [str(p) for p in sopita.find_all('p')]
+            data[enlace] = h1_etiqueta + p_etiqueta
+    except Exception as e:
+        print(f"Error al procesar el enlace {enlace}: {e}")
+
+with open('data.json', 'w') as f:
+    json.dump(data, f, indent=4, sort_keys=True)
